250ffe2391f15059c2e036c8b9c20ac856f9365b
bruno <brunoazizsm@gmail.com>
2023-10-08 13:41:43 -0300

    enderecamento indexado v[2]

---
+++ enderecamento-indexado.s
@@ -0,0 +1,10 @@
+.section .data
+v: .quad 10, 30, 5, 10, 50
+    .section .text
+    .global _start
+    _start:
+    movq $2, %rax
+    movq v(, %rax, 8), %rdi
+    movq $60, %rax
+    syscall
+    
